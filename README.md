f7b103a55ce8c198ecb9138b68512131e18f3698
Sean Corfield <sean@corfield.org>
2014-11-05 19:59:47 -0800

    Implemented stale source (from Leiningen's javac task) and multi-file compile to expected :compile-path location.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A Leiningen plugin to compile Frege code.
 
 Very much a work-in-progress right now.
 
-It's currently hardcoded to compile Hi.fr into cljbuild/Hi.java.
+It compiles Frege source (in :frege-source-path or ".") to :compile-path, but only compiles .fr to .java right now.
 
 I'm working on supporting Frege compiler options right now but just wanted this in a shared location so I can work on it from multiple machines!
 
--- src/leiningen/fregec.clj
+++ src/leiningen/fregec.clj
@@ -4,17 +4,64 @@
 ;; testing
 
 (ns leiningen.fregec
-  (:require [leiningen.classpath :as cp]))
+  (:require [clojure.java.io :as io]
+            [leiningen.classpath :as cp])
+  (:import java.io.File))
+
+(defn- stale-frege-sources
+  "Returns a lazy seq of file paths: every Frege source file within dirs modified
+  since it was most recently compiled into compile-path."
+  [dirs compile-path]
+  (for [dir dirs
+        ^File source (filter #(-> ^File % (.getName) (.endsWith ".fr"))
+                             (file-seq (io/file dir)))
+        :let [rel-source (.substring (.getPath source) (inc (count dir)))
+              rel-compiled (.replaceFirst rel-source "\\.fr$" ".class")
+              compiled (io/file compile-path rel-compiled)]
+        :when (>= (.lastModified source) (.lastModified compiled))]
+    (.getPath source)))
+
+(defn- create-opts
+  "Create options for the Frege compiler."
+  [project]
+  (let [source-path (:frege-source-path project)
+        compile-path (:compile-path project)
+        options 0 ; will fix this shortly - 0 = just compile to Java
+        prefix "" ; Frege package prefix - should always be empty string
+        classpath (cp/get-classpath-string project)]
+    (frege.compiler.Main/createopts (into-array String [source-path])
+                                    options
+                                    compile-path
+                                    (into-array String []) ; classpath
+                                    prefix)))
+
+(defn- print-opts
+  "Run the Frege code to print the compiler options."
+  [opts]
+  (->> opts
+       frege.compiler.Main/printopts
+       frege.prelude.PreludeBase$TST/performUnsafe
+       .call))
+
+(defn- compiler
+  "Create compiler object for project and options."
+  [project opts]
+  (let [files (stale-frege-sources [(:frege-source-path project)]
+                                   (:compile-path project))]
+    (frege.compiler.Main/runfregec (into-array String files)
+                                   opts
+                                   (java.io.PrintWriter. *out*))))
 
 (defn fregec
   "Compile Frege source files in :frege-source-path to :compile-path
 
 Set :fregec-options in project.clj to pass options to the Frege compiler."
   [project]
-  (let [classpath (cp/get-classpath-string project)
-        options (frege.compiler.Main/createopts (into-array String ["."]) 0 "cljbuild" (into-array String []) "")
-        compiler (frege.compiler.Main/runfregec (into-array String ["Hi.fr"])
-                                                options
-                                                (java.io.PrintWriter. *out*))
+  (let [project (merge {:frege-source-path "."
+                        :compile-path "build"}
+                       project)
+        options (create-opts project)
+        compiler (compiler project options)
         function (frege.prelude.PreludeBase$TST/performUnsafe compiler)]
+    (print-opts options)
     (.call function)))
