8a434c50d2c09cfc42f895562da5bfd8f7a589c9
Sean Corfield <sean@worldsingles.com>
2014-11-08 21:07:12 -0800

    Switch to plural source paths; clean up project.clj.

--- example/project.clj
+++ example/project.clj
@@ -1,11 +1,12 @@
 (defproject example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Example Clojure / Frege mixed language application"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
+                 ;; need to depend on Frege for runtime:
                  [com.theoryinpractise.frege/frege "3.21.586-g026e8d7"]]
-  :frege-source-path "src/frege"
   :plugins [[lein-fregec "0.1.0-SNAPSHOT"]]
+  :frege-source-paths ["src/frege"]
   :main example.core
   :profiles {:uberjar {:aot :all}})
