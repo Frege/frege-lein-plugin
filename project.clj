928e567219ed5e503fad1854f73bd1f0591add70
Sean Corfield <sean@corfield.org>
2015-02-08 20:19:37 -0800

    Cleanup - we don't actually need the Frege compiler dependency in the plugin, just in the subprocess dependencies.

--- project.clj
+++ project.clj
@@ -3,8 +3,4 @@
   :url "https://github.com/seancorfield/lein-fregec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in :leiningen
-  ;; the plugin depends on the Frege runtime in order to drive the
-  ;; compiler, but the subprocess-profile used in the plugin also
-  ;; has to manipulate the dependencies so make sure to keep this in sync!
-  :dependencies [[com.theoryinpractise.frege/frege "3.22.324-g630677b"]])
+  :eval-in :leiningen)
